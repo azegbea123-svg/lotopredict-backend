0a75d26024e299ac141e9141544b461debca7a0a
GBADOE <azegbea123@gmail.com>
2026-01-01 07:31:11 +0000

    use dynamic date for football matches

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import footballRoutes from "./routes/football.routes.js";
+
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,6 +9,8 @@ const axios = require("axios");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/football", footballRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("LotoPredict backend actif");
