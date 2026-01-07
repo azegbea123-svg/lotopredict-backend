28eb432b0b17605b75b68d1e33f216925d09a4f6
GBADOE <azegbea123@gmail.com>
2026-01-07 10:42:24 +0000

    Sauvegarde automatique 07/01/2026 10:42:24,09

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import cors from "cors";
 import footballRoutes from "./routes/football.js";
 import admin from "firebase-admin";
-import { firebaseApp } from "./firebase.js"; // ton fichier firebase.js fonctionnel
+import { firebaseApp } from "firebase.js"; // ton fichier firebase.js fonctionnel
 
 // ===============================
 // 🔐 Firebase
